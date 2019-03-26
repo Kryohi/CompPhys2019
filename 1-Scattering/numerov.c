ed17c40ccb5f288cdedb101391291c55e24b9653
Albi4 <albi41996@gmail.com>
2019-03-26 16:37:42 +0100

    Fixed integral and normalization check

--- 1-Scattering/numerov.c
+++ 1-Scattering/numerov.c
@@ -110,11 +110,11 @@ Spectrum numerov(int nmax, int l, int xmax, double rmax, double Estep, bool norm
             
             if (normalize==true)    {
                 double norm = normalizationFactor(sp.eigfuns, h, xmax*nfound, xmax*(nfound+1));
-                printf("normalization=%f", norm);
+                printf("normalization=%f\n", norm);
                 for (int x = xmax*nfound; x < xmax*(nfound+1); x++)
-                    sp.eigfuns[x] = sp.eigfuns[x] / norm;
+                    sp.eigfuns[x] = sp.eigfuns[x] /sqrt(norm);
                 double norm1 = normalizationFactor(sp.eigfuns, h, xmax*nfound, xmax*(nfound+1));
-                printf("normalization=%1.10f", norm1);
+                printf("normalization=%1.10f\n\n", norm1);
             }
                 
             nfound++;
