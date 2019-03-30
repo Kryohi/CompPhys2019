64e1d220d6f8304ad95db978b2c5dd3f1a63fbb8
Kryohi <fabio96.ck@gmail.com>
2019-03-30 23:20:08 +0100

    complete output

--- 1-Scattering/plotting.jl
+++ 1-Scattering/plotting.jl
@@ -1,4 +1,4 @@
-using Statistics, PyCall, Plots, DataFrames, CSVFiles, QuadGK
+using Statistics, Plots, DataFrames, CSVFiles, LaTeXStrings
 gr()
 
 ## CSV import
@@ -6,7 +6,7 @@ cd("./Data_scattering")
 df_E = DataFrame(load("eigenvalues.csv"))
 df_u = DataFrame(load("eigenvectors.csv"))
 
-## Plots
+## Harmonic oscillator
 l = 0
 X = LinRange(0, 12, length(df_u.y00)-2)
 u = convert(Matrix, df_u[3:end, (df_u[1,:] .== l) .& (df_u[2,:] .== 0)])
@@ -39,3 +39,29 @@ plot(X, df_u.y, label="u(x)")
 
 file = string("./scatterwave_l",6,".pdf")
 savefig(S,file)
+
+
+# total cross-section
+df_tcs = DataFrame(load("total_csection.csv"))
+tcs = plot(df_tcs.E, df_tcs.sigma_tot, label=L"\sigma_{tot}")
+savefig(tcs,"./crosssectionfine.pdf")
+
+# phase-shifts
+df_pss = DataFrame(load("phase_shifts.csv"))
+pss = plot(df_pss.E, df_pss.l0, label=L"\delta_{0}")
+plot!(df_pss.E, df_pss.l1, label=L"\delta_{1}")
+plot!(df_pss.E, df_pss.l2, label=L"\delta_{2}")
+plot!(df_pss.E, df_pss.l3, label=L"\delta_{3}")
+plot!(df_pss.E, df_pss.l4, label=L"\delta_{4}")
+plot!(df_pss.E, df_pss.l5, label=L"\delta_{5}")
+plot!(df_pss.E, df_pss.l6, label=L"\delta_{6}")
+savefig(pss,"./phase_shifts.pdf")
+
+pssc = plot(df_pss.E, sin.(df_pss.l0).^2, label=L"\delta_{0}")
+plot!(df_pss.E, sin.(df_pss.l1).^2 .*3, label=L"\delta_{1}")
+plot!(df_pss.E, sin.(df_pss.l2).^2 .*5, label=L"\delta_{2}")
+plot!(df_pss.E, sin.(df_pss.l3).^2 .*7, label=L"\delta_{3}")
+plot!(df_pss.E, sin.(df_pss.l4).^2 .*9, label=L"\delta_{4}")
+plot!(df_pss.E, sin.(df_pss.l5).^2 .*11, label=L"\delta_{5}")
+plot!(df_pss.E, sin.(df_pss.l6).^2 .*13, label=L"\delta_{6}")
+savefig(pssc,"./phase_shifts_contributions.pdf")
--- 1-Scattering/scattering.c
+++ 1-Scattering/scattering.c
@@ -4,26 +4,29 @@
 double V_lj(double x, double epsilon, double sigma);
 double V_fastlj(double dr);
 
-#define lmax 9 // 6
+#define N_E 300 // number of collision energies
+#define lmax 7 // 6
 #define rlow 0.63 // where V = 10 0.923, V=100 0.752, V=1000 0.63
 #define hbar 1.0545718e-34
 #define hbar_eV 4.13566766225e-15
 
 /* TODO 
- * 
+ * salvare tutti i phase shift, a tutte le energie in colonna
  * 
  */
 
 int main(int argc, char** argv)
 {
-    double sigma = 3.18e-10;
-    double epsilon = 5.9e-3 * 1.602176565e-19;
+    double sigma = 3.18e-10; // m
+    double epsilon = 5.9e-3 * 1.602176565e-19; // J
     double E, Emax = 0.59322; // 3.5/5.9
-    double m = 1.66054e-27/(1/83.798 + 1/1.00794); // in kg
+    double Estep = Emax/N_E;
+    double m = 1.66054e-27/(1/83.798 + 1/1.00794); // kg
     double h2m = hbar*hbar/(2*m); // in un ibrido bastardizzato SI/eV
     h2m = h2m/(epsilon*sigma*sigma); // ridotta
-    double rmax = 10.01; // Melius est abundare quam deficere
-    int xmax = 16000; // vedi sopra
+    //h2m = 0.035;
+    double rmax = 12.01; // Melius est abundare quam deficere
+    int xmax = 24000; // vedi sopra
     double h = rmax/xmax;
     int xmin = (int)round(rlow/h);   // point where V equals 1000 (chosen by looking at when the bound states become uncorrelated to this)
     printf("\nxmin is %d\n", xmin);
@@ -32,6 +35,7 @@ int main(int argc, char** argv)
     double k, a, sigma_tot = 0;
     double delta[lmax+1];
     double J1[lmax+1]; double N1[lmax+1]; double J2[lmax+1]; double N2[lmax+1];
+    double tot_csection[N_E]; // prob non serve
     
     // creates data folder and files to save data
     make_directory("Data_scattering");
@@ -40,7 +44,15 @@ int main(int argc, char** argv)
     // Files for output
     FILE * wf;
     FILE * ps;
+    FILE * pss;
+    pss = fopen("./phase_shifts.csv", "w");
+    fprintf(pss, "E, ");
+    for (int l=0; l<lmax; l++)
+        fprintf(pss, "l%d, ", l);
+    fprintf(pss, "l%d\n", lmax);
     FILE * tcs;
+    tcs = fopen("./total_csection.csv", "w");
+    fprintf(tcs, "E, sigma_tot\n");
     char filename[64];
     
     
@@ -77,18 +89,19 @@ int main(int argc, char** argv)
     
     /*   Scattering   */
     
-    double r1 = 5.0;//*sigma;
-    double r2 = r1+.5;//*sigma;
+    double r1 = 10.5;//*sigma;
+    double r2 = 11.75;//*sigma;
     double kr1, kr2;
     int x1 = (int)round(r1/h);
     int x2 = (int)round(r2/h);
     
     // Energy fixed for point 6
-    E = 0.3;
+    //E = 0.3;
 
-    
-    // precalculation of the known, constant terms
-    
+    // Ex. 7
+    for (E = 3*Estep; E<=Emax; E+=Estep)
+    {
+        // precalculation of the known, constant terms
         k = sqrt(E/h2m);
         kr1 = k*r1;
         kr2 = k*r2;
@@ -109,55 +122,65 @@ int main(int argc, char** argv)
         fast_bessel(kr2, lmax, J2);
         fast_bessel(kr2, lmax, N2);
         
-        for (int l=0; l<=lmax; l++)  {
+        /*for (int l=0; l<=lmax; l++)  {
             printf("J1[%d] = %f\n", l, J1[l]);
             printf("N1[%d] = %f\n", l, N1[l]);
             printf("J2[%d] = %f\n", l, J1[l]);
             printf("N2[%d] = %f\n", l, N2[l]);
-        }printf("\n");
+        }printf("\n");*/
 
-    for (int x=0; x<xmax; x++)  {
-        V_[x] = V_fastlj(x*h+1e-15);
-        centrifugal[x] = 1/(x*h*x*h+1e-15);
-    }
+        for (int x=0; x<xmax; x++)  {
+            V_[x] = V_fastlj(x*h+1e-15);
+            centrifugal[x] = 1/(x*h*x*h+1e-15);
+        }
     
-    // Boundary conditions near zero, see point 5
-    y[0] = 0;
-    double b = pow(4/(25*h2m), 0.1); // ~1.1635, da ricontrollare?
-    printf("b = %f\n", b);
-    for (int x=1; x<xmin; x++)
-        y[x] = exp(-pow(b/(x*h), 5));
+        // Boundary conditions near zero, see point 5
+        y[0] = 0;
+        double b = pow(4/(25*h2m), 0.1); // ~1.1635, da ricontrollare?
+        for (int x=1; x<xmin; x++)
+            y[x] = exp(-pow(b/(x*h), 5));
     
+        // Calculation of the phase shifts at various l
+        for (int l=0; l<=lmax; l++)
+        {
+            // Numerov solution
+            for (int x=0; x<xmax; x++)
+                k2[x] = (E-V_[x])/h2m - l*(l+1)*centrifugal[x];
     
-    for (int l=0; l<=lmax; l++)
-    {
-        // Numerov solution
-        for (int x=0; x<xmax; x++)
-            k2[x] = (E-V_[x])/h2m - l*(l+1)*centrifugal[x];
+            numerov_forward(h*h, xmax, xmin, k2, y);
     
-        numerov_forward(h*h, xmax, xmin, k2, y);
-    
-        a = y[x1]*r2 / (y[x2]*r1);
-        delta[l] = atan2( (a*J2[l] - J1[l]), (a*N2[l] - N1[l]) );   // phase shift
-        sigma_tot += 4*M_PI * (2*l+1) * sin(delta[l])*sin(delta[l]) / (k*k);
+            a = y[x1]*r2 / (y[x2]*r1);
+            delta[l] = atan2( (a*J2[l] - J1[l]), (a*N2[l] - N1[l]) );   // phase shift
+            sigma_tot += 4*M_PI * (2*l+1) * sin(delta[l])*sin(delta[l]) / (k*k);
         
-        snprintf(filename, 64, "./wavefunction_l%d.csv", l);
-        wf = fopen(filename, "w");
-        fprintf(wf, "Veff, y\n");
-        for (int x=0; x<xmax; x++)
-            fprintf(wf, "%f, %0.12f\n", V_[x] + h2m*l*(l+1)*centrifugal[x], y[x]);
+            /*snprintf(filename, 64, "./wavefunction_l%d.csv", l);
+            wf = fopen(filename, "w");
+            fprintf(wf, "Veff, y\n");
+            for (int x=0; x<xmax; x++)
+                fprintf(wf, "%f, %0.12f\n", V_[x] + h2m*l*(l+1)*centrifugal[x], y[x]);*/
+        }
+    
+        for (int l=0; l<=lmax; l++)
+            printf("phase shift l=%d: %f\t sin^2 = %f\n", l, delta[l], sin(delta[l])*sin(delta[l]));
+    
+        printf("scattering amplitude : %f\n\n", sigma_tot);
+        
+        // save data
+        fprintf(tcs, "%f, %f\n", E, sigma_tot);
+        fprintf(pss, "%f, ", E);
+        for (int l=0; l<lmax; l++)
+            fprintf(pss, "%f, ", delta[l]);
+        fprintf(pss, "%f\n", delta[lmax]);
+        
+    
+        /*snprintf(filename, 64, "./phaseshifts_r1%0.4f_r2%0.4f.csv", r1, r2);
+        ps = fopen(filename, "w");
+        fprintf(ps, "ph_sh, contribution, sigma_tot\n");
+        for (int l=0; l<=lmax; l++)
+            fprintf(ps, "%0.9f, %0.9f, %f\n", delta[l], (2*l+1) * sin(delta[l])*sin(delta[l]), sigma_tot);*/
+        
+        sigma_tot = 0;
     }
-    
-    for (int l=0; l<=lmax; l++)
-        printf("phase shift l=%d: %f\t sin^2 = %f\n", l, delta[l], sin(delta[l])*sin(delta[l]));
-    
-    printf("scattering amplitude : %f\n", sigma_tot);
-    
-    snprintf(filename, 64, "./phaseshifts_rlow%0.4f_rmax%0.4f.csv", rlow, r2);
-    ps = fopen(filename, "w");
-    fprintf(ps, "ph_sh, contribution, sigma_tot\n");
-    for (int l=0; l<=lmax; l++)
-        fprintf(ps, "%0.9f, %0.9f, %f\n", delta[l], (2*l+1) * sin(delta[l])*sin(delta[l]), sigma_tot);
     
     
     return 0;
